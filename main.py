97b2fdb7726f1163bc2518f8f65e1a1a9147d4af
kaufman79 <kaufman79@gmail.com>
2024-07-09 10:15:34 -0400

    add main window, pulls from SQL DB. Add dialog "add student"

--- main.py
+++ main.py
@@ -1,49 +1,104 @@
 from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, \
-    QLineEdit, QPushButton
-
+    QLineEdit, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem, \
+    QDialog, QVBoxLayout, QComboBox
+from PyQt6.QtGui import QAction
 import sys
-from datetime import datetime
+import sqlite3
 
 
 # we make a new class that inherits from QWidget. We could make an instance of QWidget instead
 # but this is the recod way, cause now we can add some new methods and override certain things.
-class AgeCalculator(QWidget):
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Age Calculator")
-        grid = QGridLayout()
+        self.setWindowTitle("Student Management System")
 
-        # we make the widgets that we will attach to the grid
-        name_label = QLabel("Name:")
-        self.name_line_edit = QLineEdit()
+        # menu items
+        file_menu_item = self.menuBar().addMenu("&File")
+        help_menu_item = self.menuBar().addMenu("&Help")
 
-        date_birth_label = QLabel("Date of Birth MM/DD/YYYY:")
-        self.date_birth_line_edit = QLineEdit()
+        # file menu actions
+        add_student_action = QAction("Add Student", self)
+        add_student_action.triggered.connect(self.insert)  # give a method as argument, but without calling it.
+        file_menu_item.addAction(add_student_action)
 
-        calculate_button = QPushButton("Calculate Age")
-        calculate_button.clicked.connect(self.calculate_age)
-        self.output_label = QLabel("")  # empty label at first but after the calculation we will populate it
+        # help menu actions
+        about_action = QAction("About", self)
+        help_menu_item.addAction(about_action)
+        about_action.setMenuRole(QAction.MenuRole.NoRole)
 
-        # now we attach those widgets to the grid
-        grid.addWidget(name_label, 0, 0)
-        grid.addWidget(self.name_line_edit, 0, 1)
-        grid.addWidget(date_birth_label, 1, 0)
-        grid.addWidget(self.date_birth_line_edit, 1, 1)
-        grid.addWidget(calculate_button, 2, 0, 1, 2)  # spans across just 1 row but 2 columns so it takes up the whole width of the window
-        grid.addWidget(self.output_label, 3, 0, 1, 2)
+        # add the table
+        self.table = QTableWidget()
+        self.table.setColumnCount(4)
+        self.table.setHorizontalHeaderLabels(("ID", "Name", "Course", "Mobile"))
+        self.table.verticalHeader().setVisible(False)  # this is the row numbers to the left of the table - we disable them
+        self.setCentralWidget(self.table)
 
-        # now we attach the grid to the Age_Calculator widget using setLayout method, inherited from the superclass
-        self.setLayout(grid)
+    def load_data(self):
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("SELECT * FROM students")
+        self.table.setRowCount(0)  # resets the table - we will be writing the data, so we want to start with a blank canvas
+        for row_number, row_data in enumerate(result):  # row data will be a tuple with the 4 column values for each row.
+            self.table.insertRow(row_number)
+            for column_number, cell_data in enumerate(row_data):
+                self.table.setItem(row_number, column_number, QTableWidgetItem(str(cell_data)))
+        connection.close()
 
-    def calculate_age(self):
-        current_year = datetime.now().year
-        date_of_birth = self.date_birth_line_edit.text()
-        year_of_birth = datetime.strptime(date_of_birth, "%m/%d/%Y").date().year
-        age = current_year - year_of_birth
-        self.output_label.setText(f"{self.name_line_edit.text()} is {age} years old")  # sending the calculation text back to the widget
+    def insert(self):
+        dialog = InsertDialog()
+        dialog.exec()
+
+
+class InsertDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Insert Student Data")
+        self.setFixedWidth(300)
+        self.setFixedHeight(300)
+
+# need a layout for this window. Could use a grid as before, but actually the
+# widgets will be all vertical, so we will use a vertical layout
+        layout = QVBoxLayout()
+
+        # add student name widget
+        self.student_name = QLineEdit()
+        self.student_name.setPlaceholderText("Name")
+        layout.addWidget(self.student_name)
+
+        # add combo box of courses
+        self.course_name = QComboBox()
+        courses = ["Biology", "Math", "Astronomy", "Physics"]
+        self.course_name.addItems(courses)
+        layout.addWidget(self.course_name)
+
+        # add mobile number widget
+        self.mobile = QLineEdit()
+        self.mobile.setPlaceholderText("Mobile")
+        layout.addWidget(self.mobile)
+
+        # add submit button
+        button = QPushButton("Submit")
+        button.clicked.connect(self.add_student)
+        layout.addWidget(button)
+
+        self.setLayout(layout)
+
+    def add_student(self):
+        name = self.student_name.text()
+        course = self.course_name.itemText(self.course_name.currentIndex())
+        mobile = self.mobile.text()
+        connection = sqlite3.connect("database.db")
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO students (name, course, mobile) Values (?, ?, ?)",
+                       (name, course, mobile))
+        connection.commit()
+        cursor.close()
+        connection.close()
+        main_window.load_data()
 
 
 app = QApplication(sys.argv)
-age_calculator = AgeCalculator()
-age_calculator.show()
+main_window = MainWindow()
+main_window.show()
+main_window.load_data()
 sys.exit(app.exec())
